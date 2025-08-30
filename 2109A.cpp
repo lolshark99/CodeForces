035e6f1abab9db9c34a65cc9dc017c48a0d3d106
Siddhaarth <siddhaarthsharma360@gmail.com>
2025-08-30 23:21:41 +0530

    Add files via upload

--- 2109A.cpp
+++ 2109A.cpp
@@ -1,29 +1,43 @@
 #include<iostream>
 using namespace std;
-
+ 
 void solve(int t){
     for(int i = 0 ; i < t ; i++){
         int n;
         cin >> n;
         int arr[n];
+        int ones_count = 0;
         for(int j = 0 ; j < n ; j++){
             cin >> arr[j];
         }
-        int k;
-        for(k = 0 ; k < n-1 ; k++){
-            if(arr[k] == 0 && arr[k+1] == 0){
-                cout << "Yes" << endl;
-                break;               
+        for(int k = 0 ; k < n; k++){
+            if(arr[k] == 1){
+                ones_count++;
             }
         }
-        if(k == n-1){                
-            cout << "No" << endl;
+        if(ones_count == n){
+            cout << "YES" << endl;
+            continue; 
+        }
+        int found_0 = 0;
+        for(int l = 0 ; l < n - 1; l++){
+            if(arr[l] == 0 && arr[l+1] == 0){
+                found_0 = 1;
+                break;
+            }
+        }
+        if(found_0 == 1){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
         }
     }
 }
-
+ 
 int main(){
     int t;
-    cin>>t;
+    cin >> t;
     solve(t);
+    return 0;
 }
