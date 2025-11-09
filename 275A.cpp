dd22fe16571ca123fa80185b9cd87e92ad39ccb3
Siddhaarth <siddhaarthsharma360@gmail.com>
2025-11-09 22:17:37 +0530

    Add files via upload

---
+++ 275A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solver(){
+    int n ;
+    int m ;
+    int switch_pos[n][m];
+    //basic idea is that if the no of switches is even thn no change else we flip it and the adjacent pos!!!
+    for(int i = 0 ; i < 3 ;i++){
+        for(int j = 0 ; 3 < m ;j++){
+            cin>>switch_pos[i][j];
+        }
+    }
+}
