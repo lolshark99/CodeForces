e638a3d5187502d209c9c411807fb60bf47941c3
Siddhaarth <siddhaarthsharma360@gmail.com>
2026-01-01 22:48:12 +0530

    Add files via upload

---
+++ 1837B.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solver(){
+    int n;
+    string s;
+    cin>>n;
+    cin>>s;
+    
+
+    int current_len = 0;
+    int max_len = 0;
+
+    for(int i  = 1 ; i < s.size() ;i++){
+        if(s[i] == s[i-1]){
+            current_len++;
+            max_len = max(current_len , max_len);
+        }
+        else{
+            current_len = 0;
+        }
+    }
+    cout<<max_len + 2<<endl; // min cost will be 2 always!!
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solver();
+    }
+}
