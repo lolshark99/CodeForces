d33b2ed753e4ded23cbbcf63deab95ec6bbb8c22
Siddhaarth <siddhaarthsharma360@gmail.com>
2026-01-02 23:21:42 +0530

    Refactor code with additional comments
    Added comments for better understanding of the logic.

--- 1837B.cpp
+++ 1837B.cpp
@@ -9,7 +9,7 @@ void solver(){
     
     int current_len = 0;
     int max_len = 0;
-
+    // check for max_len for the substring with the same operator.
     for(int i  = 1 ; i < s.size() ;i++){
         if(s[i] == s[i-1]){
             current_len++;
@@ -19,8 +19,8 @@ void solver(){
             current_len = 0;
         }
     }
-    // basically we needd new nums when we have the same operator and len of the longest substring will be the no of the nums needed!! 
-    cout<<max_len + 2<<endl; // min cost will be 2 always!!
+    // basically we need new nums when we have the same operator and len of the longest substring will be the no of the nums needed!! 
+    cout<<max_len + 2<<endl; // min cost will be 2 always as 1 element array with < , > ops is not possible.
 }
 
 int main(){
@@ -30,3 +30,4 @@ int main(){
         solver();
     }
 }
+
