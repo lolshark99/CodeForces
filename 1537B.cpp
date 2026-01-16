d7c1d84192a030d4034778f90abcb453b09a083b
Siddhaarth <siddhaarthsharma360@gmail.com>
2026-01-17 00:02:35 +0530

    Add files via upload

---
+++ 1537B.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solver(){
+    int m , n , i, j;
+    cin>>n>>m>>i>>j;
+    long long x1 , y1 , x2, y2;
+    x1 = 1;
+    y1 = 1;
+    x2 = n;
+    y2 = m;
+    cout<<x1<<" "<<y1<<" "<<x2<<" "<<y2<<endl;
+}
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        solver();
+    }   
+}
