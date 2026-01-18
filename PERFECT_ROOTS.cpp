136468976acdb7ed8850178daf7fb36b8ec34fd0
Siddhaarth <siddhaarthsharma360@gmail.com>
2026-01-18 22:54:17 +0530

    Add files via upload

---
+++ PERFECT_ROOTS.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solver(){
+    int n;
+    cin>>n;
+    for(int i = 1 ; i <= n ;i++){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+} 
+
+int main(){
+    int t;
+    while(t--){
+        solver();
+    }
+}
