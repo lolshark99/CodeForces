44a7f71097309d370c30e13713bf456beeae330f
Siddhaarth <siddhaarthsharma360@gmail.com>
2026-01-21 22:44:30 +0530

    Add files via upload

--- 2164B.cpp
+++ 2164B.cpp
@@ -8,18 +8,28 @@ void solver(){
         cin>>numbers[i];
     }
     // if diff between any 2 nos is even then its mod is even also!!
-    bool flag = 0;
-    for(int j = 0 ;  j < n ; j++){
-        for(int k = j + 1 ; k < n ;k++){
-            if((numbers[k] - numbers[j]) % 2 == 0){
-                cout<<numbers[j]<<" "<<numbers[k]<<endl;
-                flag = 1;
-                return;
+    for(int j = 0 ; j < n ;j++){
+        int ans1 , ans2;
+        if(numbers[j] %2 == 0){
+            int ans1 = numbers[j];
+            for(int k = 1 ; k < n ;k++){
+                if(numbers[k] % 2 == 0){
+                    int ans2 = numbers[k];
+                }
             }
         }
-    }
-    if(!flag){
-        cout<<-1<<endl;
+        else if(numbers[j] %2 != 0){
+            int ans1 = numbers[j];
+            for(int k = 1 ; k < n ;k++){
+                if(numbers[k] %2 != 0){
+                    int ans2 = numbers[k];
+                }
+            }
+            cout<<ans1<<" "<<ans2<<endl;
+        }
+        else{
+            cout<<-1<<endl;
+        }
     }
 }
 
