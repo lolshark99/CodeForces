3935cb2e3169c2f88b12afeef0eb69267dfa4db6
Siddhaarth <siddhaarthsharma360@gmail.com>
2026-01-24 23:58:27 +0530

    Add files via upload

--- 2164B.cpp
+++ 2164B.cpp
@@ -4,28 +4,14 @@ void solver(){
     int n;
     cin>>n;
     vector<int>nums(n);
-    vector<int>even_nums;
-    vector<int>odd_nums;
     for(int i = 0 ; i < n ;i++){
         cin>>nums[i];
-        if(nums[i] % 2 == 0){
-            even_nums.push_back(nums[i]);
-        }
-        else{
-            odd_nums.push_back(nums[i]);
-        }
     }
-    if(even_nums.size() >= 2){
-        cout<<even_nums[0]<<" "<<even_nums[1]<<endl;
-        return;
-    }
-    else{
-        for(int j = 0 ; j < odd_nums.size() ;j++){
-            for(int k = j + 1 ; k < odd_nums.size() ;k++){
-                if(odd_nums[k] % odd_nums[j] == 0 || (odd_nums[k] % odd_nums[j])% 2 == 0){
-                    cout<<odd_nums[j]<<" "<odd_nums[k]<<endl;
-                    return;
-                }
+    for(int j = 0 ; j < n ;j++){
+        for(int k = j + 1 ; k < n ;k++){
+            if((nums[k] % nums[j]) % 2 == 0){
+                cout<<nums[j]<<" "<<nums[k]<<endl;
+                return;
             }
         }
     }
