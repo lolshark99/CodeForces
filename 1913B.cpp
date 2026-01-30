e093d2f3c299687213ab6592babb124f593ca91a
Siddhaarth <siddhaarthsharma360@gmail.com>
2026-01-30 21:13:28 +0530

    Add files via upload

--- 1913B.cpp
+++ 1913B.cpp
@@ -3,28 +3,32 @@ using namespace std;
 void solver(){
     string s;
     cin>>s;
-    int count0 = 0;
     int count1 = 0;
+    int count0 = 0;
     for(char c : s){
-        if(c == '1'){
-            count1++;
+        if(c  == '0'){
+            count0++;
         }
         else{
-            count0++;
+            count1++;
         }
     }
     int n = s.size();
-    //int r = min(count0 , count1);
-    int r = abs(count0 - count1); 
-    if(n == 1){
-        cout<<1<<endl;
+    int excess = 0;
+    for(int i = 0 ; i , n ;i++){
+        if(s[i] == '0' && count1 > 0){
+            excess++;
+            count1--;
+        }
+        else if(s[i] == '1' && count0 > 0){
+            excess++;
+            count0--;
+        }
+        else{
+            break;
+        }
     }
-    else if(count0 == count1){
-        cout<<0<<endl;
-    }
-    else{
-        cout<<n - r<<endl;
-    }
+    cout<<n - excess<<endl;
 }
 int main(){
     ios::sync_with_stdio(false);
