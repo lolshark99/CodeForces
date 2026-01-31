5ecb0e90ef8b8a322660b127cd0515a196f88889
Siddhaarth <siddhaarthsharma360@gmail.com>
2026-01-31 23:31:08 +0530

    Add files via upload

--- 58A.cpp
+++ 58A.cpp
@@ -4,13 +4,15 @@ void solver(){
     string s;
     cin>>s;
     string ans = "hello";
-    int j = 0 ;
-    for(char c : s){
-        if(c == s[j]){
+    int j = 0;
+    for(int i = 0 ; i < s.size() ;i++){
+        if(s[i] == ans[j]){
             j++;
+            continue;
         }
-        if(j == ans.size()){
-            break;
+        else{
+            //j = 0;
+            continue;
         }
     }
     if(j == ans.size()){
