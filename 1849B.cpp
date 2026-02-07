0b719c42bb5643237a204b1e357017bf7f168345
Siddhaarth <siddhaarthsharma360@gmail.com>
2026-02-08 00:26:29 +0530

    Add files via upload

--- 1849B.cpp
+++ 1849B.cpp
@@ -1,34 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-void solve_rec(int j, int n, vector<long long>& health, vector<int>& order) {
-    if (j == n) return; 
-    if (health[j] == 0) {
-        order.push_back(j);
-        solve_rec(j + 1, n, health, order);
-    } else {
-        solve_rec(j + 1, n, health, order);
-    }
-}
-void solver() {
-    int n, k;
+
+void solver(){
+    int n , k;
     cin >> n >> k;
-    vector<long long> health(n);
-    for (int i = 0; i < n; i++) {
+    vector<pair<int , int>> index_map(n);
+    vector<int> health(n);
+    for(int i = 0 ; i < n ; i++){
         cin >> health[i];
         health[i] %= k;
+        if(health[i] == 0) health[i] = k;   
+        index_map[i].first = health[i];
+        index_map[i].second = i;
     }
-    vector<int> order;
-    solve_rec(0, n, health, order);
-    for (int x : order){
-        cout<<x;
+    sort(index_map.begin(), index_map.end(), [](pair<int,int> a, pair<int,int> b){
+        if(a.first != b.first) return a.first > b.first; 
+        return a.second < b.second;                     
+    });
+    for(auto p : index_map){
+        cout << p.second + 1 << " ";
     }
     cout << endl;
 }
 
-
 int main(){
     int t;
-    cin>>t;
+    cin >> t;
     while(t--){
         solver();
     }
