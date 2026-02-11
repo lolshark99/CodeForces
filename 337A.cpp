3b067e770353d77dfb407ac9fae626a86fc33d61
Siddhaarth <siddhaarthsharma360@gmail.com>
2026-02-12 01:59:38 +0530

    Add files via upload

--- 337A.cpp
+++ 337A.cpp
@@ -4,9 +4,16 @@ int main(){
     int n , m;
     cin>>n>>m;
     vector<long long>puzzle(m);
-    for(int i = 0 ; i < n ; i++){
+    for(int i = 0 ; i < m ; i++){
         cin>>puzzle[i];
     }
-    sort(puzzle.rbegin() , puzzle.rend());
-    cout<<puzzle[0] - puzzle[1]<<endl;
+    sort(puzzle.begin() , puzzle.end());
+    //cout<<puzzle[0] - puzzle[1]<<endl;
+    
+    long long ans = LLONG_MAX;
+
+    for(int i = 0 ; i <= m - n ; i++){
+        ans = min(ans, puzzle[i + n - 1] - puzzle[i]);
+    }
+    cout <<ans<< endl;
 }
