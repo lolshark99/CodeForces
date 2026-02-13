7f54862aa483f53d936b83ca0d474cd05d96d18c
Siddhaarth <siddhaarthsharma360@gmail.com>
2026-02-13 22:36:18 +0530

    Add files via upload

--- 1543A.cpp
+++ 1543A.cpp
@@ -1,17 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
-void solver(){
+/* void solver(){
     long long a;
     long long b;
     cin>>a>>b;
     if(a == b){
-        cout<<"0"<<" "<<"0"<<endl;
+        cout<<0<<" "<<0<<endl;
     }
     else{
-        long long max_excitement = abs(a - b);
-        long long min_moves = min(a , b);
-        cout<<max_excitement<<" "<<min_moves<<endl;
+        long long max_excitment = abs(a - b); 
+        long long min_moves = min(a%max_excitment , max_excitment - a%max_excitment);
+        cout<<max_excitment<<" "<<min_moves<<endl;
     }
+} */
+
+void solver(){
+    long long a,b;
+    cin>>a>>b;
+
+    if(a == b){
+        cout << a << " " << 0 << endl;
+        return;
+    }
+
+    long long d = abs(a - b);
+    long long r = a % d;
+
+    long long min_moves = min(r, d - r);
+
+    cout<<d<<" "<<min_moves<<endl;
 }
 
 int main(){
